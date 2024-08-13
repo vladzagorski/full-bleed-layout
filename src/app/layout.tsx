a3bbceddb238b1d29852b8b48b548b80e68f2b97
Vladimir Zagorski <vlad@st6.io>
2024-08-13 15:20:55 +0300

    clean-up template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { type ReactNode } from "react";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,11 +14,13 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
